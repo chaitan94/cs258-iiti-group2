c9df10dbc1effc3d69e6f57acdb4642034fd6c48
B Krishna Chaitanya <bkchaitan94@gmail.com>
2014-02-24 21:57:14 +0530

    new tender frontend

--- pub/views/tenders/new.php
+++ pub/views/tenders/new.php
@@ -11,7 +11,18 @@ include 'views/nav.php';
 <body>
 <div class="not-nav">
 <main>
-	<h3>New tender</h3>
+	<form class="pure-form pure-form-aligned">
+		<legend>New tender</legend>
+		<fieldset>
+			<div class="pure-control-group"><label>Tender pdf</label><input type="file"></div>
+			<div class="pure-control-group"><label>Tender Name</label><input type="text"></div>
+			<div class="pure-control-group"><label>Closing date</label><input type="date"></div>
+			<div class="pure-control-group"><label>Closing time</label><input type="time" value="00:00"></div>
+			<div class="pure-control-group"><label>Category</label><input type="text"></div>
+			<div class="pure-control-group"><label>Brief</label><input type="text"></div>
+			<div class="pure-controls"><input class="pure-button pure-button-primary" type="submit" value="Confirm and proceed to create form"></div>
+		</fieldset>
+	</form>
 </main>
 </div>
 </body>
