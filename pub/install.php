3047cdae903444ad78b8ea195d638660a8c9e09e
B Krishna Chaitanya <bkchaitan94@gmail.com>
2014-03-22 18:14:48 +0530

    install script updates schema

--- pub/install.php
+++ pub/install.php
@@ -8,6 +8,7 @@
 Install script for the website<br>
 Initializes the database<br>
 Should be run atleast once<br>
+<b style="color:red">Note: this will update your database schema but data stored inside will be lost</b><br>
 <br>
 Input your database settings below:<br>
 <form method="POST">
@@ -30,6 +31,8 @@ Input your database settings below:<br>
 </table>
 </form>
 <?php
+session_start();
+session_destroy();
 if(isset($_POST['host']) && isset($_POST['user']) && isset($_POST['pass'])){
   $db = $_POST;
   try{
@@ -39,12 +42,31 @@ if(isset($_POST['host']) && isset($_POST['user']) && isset($_POST['pass'])){
   }
   $dbname = 'eprocdb';
 
-$schema = <<<END
--- Database: `$dbname`
-CREATE DATABASE IF NOT EXISTS `$dbname`;
+  $sqldb = "-- Database: `$dbname`
+CREATE DATABASE IF NOT EXISTS `$dbname`;";
+  if($db->query($sqldb)){
+    echo "<b>Database name is `$dbname`.<br></b>";
+  }else{
+    echo "<b style='color:red;'>Initialization of database failed!</b>";
+    die(var_dump($db->errorInfo()));
+  }
 
--- Table structure for table `$dbname`.`tenders`
-CREATE TABLE IF NOT EXISTS `$dbname`.`tenders` (
+  function initTable($db,$dbname,$sql,$tablename){
+    if($db->query("DROP TABLE `$dbname`.$tablename")){
+      echo "<b>old $tablename table dropped.<br></b>";
+    }
+    if($db->query("CREATE TABLE ".$sql)){
+      echo "<b>$tablename table created.<br></b>";
+      return true;
+    }else{
+      echo "<b style='color:red;'>$tablename could not be created/updated.<br></b>";
+      echo "<b>Error log:</b><br>";
+      die(var_dump($db->errorInfo()));
+      return false;
+    }
+  }
+
+  $sqltenders = "`$dbname`.`tenders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` text,
   `brief` text,
@@ -55,10 +77,10 @@ CREATE TABLE IF NOT EXISTS `$dbname`.`tenders` (
   `closedate` text,
   `closetime` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+);";
+  initTable($db,$dbname,$sqltenders,"tenders");
 
--- Table structure for table `$dbname`.`users`
-CREATE TABLE IF NOT EXISTS `$dbname`.`users` (
+  $sqlusers = "`$dbname`.`users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text,
   `pass` text,
@@ -66,15 +88,16 @@ CREATE TABLE IF NOT EXISTS `$dbname`.`users` (
   `phone` text NOT NULL,
   `type` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
-END;
+);";
+  initTable($db,$dbname,$sqlusers,"users");
 
-  if($db->query($schema)){
-    echo "<b>Database initialized successfully</b>";
-  }else{
-    echo "<b style='color:red;'>Database initialization failed!</b>";
-    var_dump($db->errorInfo());
-  }
+  $sqltenderuser = "`$dbname`.`tender_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tenderid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);";
+  initTable($db,$dbname,$sqltenderuser,"tender_user");
   $db = null;
 }
 ?>
