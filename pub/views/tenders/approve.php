0d8682f8a209e07f0938494832339084c1739151
Diksha Golait <gdiksha9414@gmail.com>
2014-03-25 23:49:21 +0530

    Update approve.php

--- pub/views/tenders/approve.php
+++ pub/views/tenders/approve.php
@@ -1,15 +1,81 @@
 <!DOCTYPE html>
 <html>
 <head>
-<?php include 'views/nav.php'; ?>
+<style type="text/css">
+.row {
+	vertical-align: top;
+	height: auto !important;
+}
+
+.list {
+	display: none;
+}
+
+.show {
+	display: none;
+}
+
+.hide:target+.show {
+	display: inline;
+}
+
+.hide:target {
+	display: none;
+}
+
+.hide:target  ~ .list {
+	display: inline;
+}
+
+.link {
+	color: #321212;
+}
+
+
+
+@media print {
+	.hide,.show {
+		display: none;
+	}
+}
+</style>
+
+<?php include 'views/nav.php';
+include 'models/tenders.php'; $i=0;?>
 	<title>Approve | Tender</title>
-	<link rel="stylesheet" type="text/css" href="/css/pure.min.css">
-	<link rel="stylesheet" type="text/css" href="/css/base.css">
+<link rel="stylesheet" type="text/css" href="/css/pure.min.css">
+<link rel="stylesheet" type="text/css" href="/css/base.css">
 </head>
 <body>
-<div class="not-nav">
-<main>
-</main>
-</div>
+	<div class="not-nav">
+		<main>
+		<ul>
+		
+<?php $tender= new Tender();
+$r= $tender.getTendersByOwner();?>
+<?php foreach ($r as $key => $value):?>
+
+<p><p><li><a href='#' ><?php echo 'Tender Document for '.$value->title;$i++;?></a></li>
+<?php echo '&nbsp&nbspClosing date:';?> </p>
+<div class="row">
+ <a  href=<?php echo'#hide'.$i?>  class="hide" id=<?php echo'hide'.$i?>><font class="link">View applications</font></a>
+ <a  href=<?php echo'#show'.$i?> class="show" id=<?php echo'show'.$i?>><font class="link">Hide Aplications</font></a>
+ <div class="list">
+ <ul>
+ <?php $item=new Tender($value->id);
+ $tender_applicants= $item.getApplicants();
+ $no_applicants=count($tender_applicants);?>
+ <?php while($no_applicants>0):?>
+ <li> <?php echo $tender_applications->users.name;?></li>
+ <?php $no_applicants--;?>
+<?php endwhile;?>
+ </ul><br>
+ </div>
+ </div></p>
+<?php endforeach;?>
+</ul>
+		</main>
+	</div>
 </body>
 </html>
+
