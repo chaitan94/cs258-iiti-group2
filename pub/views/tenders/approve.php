9d197fa51251ea6e26acf34a545f3d74ba337f44
B Krishna Chaitanya <bkchaitan94@gmail.com>
2014-04-07 18:16:32 +0530

    slight front end in approve tenders page

--- pub/views/tenders/approve.php
+++ pub/views/tenders/approve.php
@@ -4,41 +4,45 @@
 <?php include 'views/nav.php';
 include 'models/tenders.php'; $i=0;?>
 	<title>Approve | Tender</title>
-<link rel="stylesheet" type="text/css" href="/css/tender-approve.css">
 <link rel="stylesheet" type="text/css" href="/css/pure.min.css">
 <link rel="stylesheet" type="text/css" href="/css/base.css">
+<link rel="stylesheet" type="text/css" href="/css/tender-approve.css">
 </head>
 <body>
 <div class="not-nav">
 <main>
-<ul>	
-<?php $tender= new Tender();
-$r= $tender->getTendersByOwner($_SESSION['id']);?>
-<?php foreach ($r as $key => $value):?>
-<p>
-	<p>
-		<li><a href='#' ><?php echo 'Tender Document for '.$value->title;$i++;?></a></li>
-		<?php echo '&nbsp&nbspClosing date:';?> 
-	</p>
-	<div class="row">
-		<a href="<?php echo'#hide'.$i?>" class="hide" id="<?php echo'hide'.$i?>"><font class="link">View applications</font></a>
-		<a href="<?php echo'#show'.$i?>" class="show" id="<?php echo'show'.$i?>"><font class="link">Hide Aplications</font></a>
-		<div class="list">
-			<ul>
-			<?php $item = new Tender($value->id);
+<div class="tender-applications">
+<?php
+$tender= new Tender();
+$r= $tender->getTendersByOwner($_SESSION['id']);
+foreach ($r as $key => $value){ 
+	$i++;
+?> 
+	<div class="tender-application">
+		<a href="#"><div class="title">Tender #<?=$value->id?>: <?=$value->title?></div></a>
+		Closing date:
+		<div class="row">
+		<?php 
+			$item = new Tender($value->id);
 			$tender_applicants = $item->getApplicants();
 			$no_applicants = count($tender_applicants);
+		?>
+			<a href="#hide<?=$i?>" class="hide" id="hide<?=$i?>"><font class="link">View applications (<?=$no_applicants?>)</font></a>
+			<a href="#show<?=$i?>" class="show" id="show<?=$i?>"><font class="link">Hide Aplications</font></a>
+			<div class="list">
+				<ul>
 
-			while($no_applicants > 0){
-				echo '<li><a href="/user/'.$tender_applicants[$no_applicants-1]->userid.'">'.$tender_applicants[$no_applicants-1]->name.'</a> - <a href="/tenders/applications/'.$tender_applicants[$no_applicants-1]->id.'">View Application</a></li>';
-				$no_applicants--;
-			} ?>
-			</ul><br>
+				<?php
+				while($no_applicants > 0){
+					echo '<li><a href="/user/'.$tender_applicants[$no_applicants-1]->userid.'">'.$tender_applicants[$no_applicants-1]->name.'</a> - <a href="/tenders/applications/'.$tender_applicants[$no_applicants-1]->id.'">View Application</a></li>';
+					$no_applicants--;
+				} ?>
+				</ul><br>
+			</div>
 		</div>
 	</div>
-</p>
-<?php endforeach;?>
-</ul>
+<?php } ?>
+</div>
 </main>
 </div>
 </body>
