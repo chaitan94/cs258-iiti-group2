b3be44d01ac4d3348b4a6276c5a21ecb9f9778ec
B Krishna Chaitanya <bkchaitan94@gmail.com>
2014-04-08 20:54:21 +0530

    Added basic frontend for tender application page shows SOQ and questionnaire

--- pub/views/tenders/apply.php
+++ pub/views/tenders/apply.php
@@ -13,13 +13,53 @@ $ten = new Tender($dno);
 <body>
 <div class="not-nav">
 <main>
-	<h3>Application for Tender #<?=$ten->id?>: <?=$ten->title?></h3>
-	<a href="/tenders/<?=$dno?>">
-		<input class="pure-button" type="button" value="Back To Details">
-	</a>
-	<form method="POST" action="/tenders/<?=$dno?>" class="pure-form">
-		<input type="text" placeholder="Name" name="name"><br>
-		<input type="submit" class="pure-button pure-button-primary"><br>
+	<form class="pure-form pure-form-aligned" method="POST" action="/tenders/<?=$dno?>">
+	<legend>Application for Tender #<?=$ten->id?>: <?=$ten->title?></legend>
+		<a href="/tenders/<?=$dno?>">
+			<input class="pure-button" type="button" value="Back To Details">
+		</a>
+		<fieldset>
+			<legend>SOQ</legend>
+			<table class="pure-table">
+			<thead><tr><th>Specification</th><th>Quantity</th><th>EMD</th><th>Bid</th></tr></thead>
+			<tbody>
+			<?php 
+			if(file_exists("data/tenders/$ten->id/soq.json")){
+				$f = fopen("data/tenders/$ten->id/soq.json", "r");
+				$soq = json_decode(fread($f, 1000));
+				fclose($f);
+				foreach ($soq as $key => $value) {
+					echo "<tr><td>$value->specification</td><td>$value->quantity</td><td>$value->emd</td><td><input type='text' required></td></tr>";
+				};
+			}else{
+				echo "SOQ unavailable";
+			}
+			?>
+			</tbody>
+			</table>
+		</fieldset>
+		<fieldset>
+			<legend>Questionnaire</legend>
+			<?php
+			if(file_exists("data/tenders/$ten->id/questionnaire.json")){
+				$f = fopen("data/tenders/$ten->id/questionnaire.json", "r");
+				$questionnaire = json_decode(fread($f, 1000));
+				fclose($f);
+				foreach ($questionnaire as $key => $value) {
+					echo "<div><h4>$value->question</h4>";
+					foreach ($value->options as $k => $v) {
+						echo "<input type='radio' name='q$key' value='$k'>$v->option<br>";
+					}
+					echo "</div>";
+				}
+			}else{
+				echo "Questionnaire unavailable";
+			}
+			?>
+		</fieldset>
+		<fieldset>
+			<div class="pure-controls"><input type="submit" class="pure-button pure-button-primary"></div>
+		</fieldset>
 	</form>
 </main>
 </div>
