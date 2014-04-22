7f4a8dd9b2d2190138b1368332b15470e23452ea
Vidyaranya <vidyaranya.ns@gmail.com>
2014-04-22 21:03:01 +0530

    Added contact page [Issue #1]

--- pub/views/contact.php
+++ pub/views/contact.php
@@ -4,13 +4,114 @@
 	<title>E Procurement</title>
 	<link rel="stylesheet" type="text/css" href="/css/pure.min.css">
 	<link rel="stylesheet" type="text/css" href="/css/base.css">
+	<link rel="stylesheet" type="text/css" href="/css/contact_style.css">
+	<link rel="stylesheet" type="text/css" href="/css/contact.css">
 </head>
 <body>
 <?php include 'nav.php'; ?>
 <div class="not-nav">
 <main>
-Phone number:
-E-mail Address:
+<div id="art-page-background-glare">
+    <div id="art-page-background-glare-image"> </div>
+</div>
+<div id="art-main">
+    <div class="art-sheet">
+        <div class="art-sheet-tl"></div>
+        <div class="art-sheet-tr"></div>
+        <div class="art-sheet-bl"></div>
+        <div class="art-sheet-br"></div>
+        <div class="art-sheet-tc"></div>
+        <div class="art-sheet-bc"></div>
+        <div class="art-sheet-cl"></div>
+        <div class="art-sheet-cr"></div>
+        <div class="art-sheet-cc"></div>
+        <div class="art-sheet-body">
+            <div class="art-header">
+                <div class="art-header-clip">
+                <div class="art-header-center">
+                    <div class="art-header-png"></div>
+                    <div class="art-header-jpeg"></div>
+                </div>
+                </div>
+                <div class="art-logo">
+                                 <h1  > Contact   Us </h1>
+                 </div>
+            </div>
+   
+<div class="art-content-layout">
+                <div class="art-content-layout-row">
+                    <div class="art-layout-cell art-sidebar1">
+<div class="art-vmenublock">
+    <div class="art-vmenublock-body">
+                <div class="art-vmenublockheader">
+                    <h3 class="t">LINKS</h3>
+                </div>
+                <div class="art-vmenublockcontent">
+                    <div class="art-vmenublockcontent-body">
+         <ul class="art-vmenu">
+        	
+        	<li >
+        	<a href="http://facebook.com"><span class="l"></span><span class="r"></span><span class="t">Home Page</span></a>
+        	</li>	
+            <li>
+            <a href="http://facebook.com"><span class="l"></span><span class="r"></span><span class="t">New Tenders</span></a>
+            </li>
+        </ul>
+         <div class="cleared"></div>
+     </div>
+  </div>
+		<div class="cleared"></div>
+</div>
+</div>
+
+                      <div class="cleared"></div>
+</div>
+<div class="art-layout-cell art-content">
+     <div class="art-post">
+         <div class="art-post-body">
+              <div class="art-post-inner art-article">
+                                
+                <div class="cleared"></div>
+   <div class="art-brahmanandam">
+        <p><img src="/img/contact/abhishek.jpg"  height="200" width="200" alt="an image" style="float:left" /><br>Saroj K. Mallick</p>
+        <div class="for_saroj_contacts" style="padding-left=10px">Manager<br>IET Campus<br>Phone-0731-24242424242424</div>
+   </div>
+                <div class="cleared"></div>
+                </div>
+    <br><br><br>
+    <div class="art-brahmanandam">
+        <p><img src="/img/contact/abhishek.jpg"  height="200" width="200" alt="an image" style="float:left; padding-left:7px;padding-right:10px " /><br>Abishek Srivatsava </p>
+        <div class="for_saroj_contacts" style="padding-left=1000000px">Manager<br>IET Campus<br>Phone-0731-24242424242424</div>
+    </div>
+
+		<div class="cleared"></div>
+       </div>
+   </div>
+
+    <div class="cleared"></div>
+         </div>
+    </div>
+</div>
+            <div class="cleared"></div>
+            <div class="art-footer">
+                <div class="art-footer-t"></div>
+                <div class="art-footer-l"></div>
+                <div class="art-footer-b"></div>
+                <div class="art-footer-r"></div>
+                <div class="art-footer-body">
+ 
+                            <div class="art-footer-text">
+                                <p style="text-align=center; padding-top=2px">Copyright © 2014.<br> Indian Institute of Technology Indore</p>
+                                                            </div>
+                    <div class="cleared"></div>
+                </div>
+            </div>
+    		<div class="cleared"></div>
+        </div>
+    </div>
+    <div class="cleared"></div>
+  </div>
+    <div class="cleared"></div>
 </main>
 </div>
 </body>
